2c0c69c29e66cf77bfbeb7080c5210e2fea64767
Nguyen-VietAnh <21020277@vnu.edu.vn>
2023-05-01 01:23:20 +0700

    update filter and search

--- src/controllers/movie/movies.controller.js
+++ src/controllers/movie/movies.controller.js
@@ -1,6 +1,6 @@
 const models = require('../../models/index');
 const movie = models.movie;
-const gerne = models.gerne;
+const Gerne = models.gerne;
 const category = models.category;
 const producer = models.producer;
 const { Op } = require('sequelize');
@@ -13,6 +13,60 @@ module.exports.getAllMovies = async (req, res) => {
 
     res.render('movie/movies', { title: 'All Movies', movies});
 
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error');
+  }
+};
+
+
+module.exports.filterMovies = async (req, res) => {
+  const { year, gerne, country } = req.query;
+
+  try {
+    const movies = await movie.findAll({
+      where: {
+        release: {
+          [Op.between]: [year ? `${year}-01-01` : '0001-01-01', year ? `${year}-12-31` : '9999-12-31']
+        }
+      },
+      include: [
+        {
+          model: producer,
+          where: {
+            country: country || { [Op.ne]: null }
+          }
+        },
+        {
+          model: Gerne,
+          where: {
+            name: gerne || { [Op.ne]: null }
+          }
+        }
+      ]
+    });
+    res.render('movie/movies', { title: 'All Movies', movies });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+
+// GET /search
+module.exports.search = async (req, res) => {
+  const { keyword } = req.query;
+  try {
+    const results = await movie.findAll({
+      where: {
+        title: {
+          [Op.like]: `%${keyword}%`
+        }
+      }
+    });
+    res.render('movie/movies', {
+      title: 'Search Results',
+      results: results || [] 
+    });
   } catch (err) {
     console.log(err);
     res.status(500).send('Server Error');
--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movie/movies.controller');
+const controller = require('../controllers/home.controller');
 
 router.get('/', movieController.getAllMovies);
+router.get('/filter', movieController.filterMovies);
+router.get('/search', controller.search);
 
 
 module.exports = router;
