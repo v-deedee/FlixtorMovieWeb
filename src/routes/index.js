8f06499c2d4c62ed18fd477cf535a851768267e9
Duc Hoang <104043038+duchoang9@users.noreply.github.com>
2023-05-01 15:24:04 +0700

    fix conflict'

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,11 @@
 const homeRouter = require("./home.route");
 const accountRouter = require("./account.route");
 const managementRouter = require("./management.route");
-const movieRouter = require('./movie.route');
 
 function routes(app) {
   app.use("/", homeRouter);
   app.use("/account", accountRouter);
   app.use("/management", managementRouter);
-  app.use('/movie', movieRouter);
 }
 
 module.exports = routes;
