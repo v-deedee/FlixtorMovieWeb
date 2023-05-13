0ba7073fe56817d5a97caf5d700f38de0b45b519
DucHoang <taduchoang2002@gmail.com>
2023-05-14 02:58:43 +0700

    update manage user, comment

--- src/controllers/management/manage.user.controller.js
+++ src/controllers/management/manage.user.controller.js
@@ -3,7 +3,13 @@ const models = require("../../models/index");
 const User = models.user;
 const Sequelize = models.sequelize;
 
+const banValue = 5;
+
 module.exports.showUsers = async (req, res) => {
+  const admin = await User.findByPk(req.userId, {
+    attributes: ["id", "user_name", "email", "password", "role"],
+  });
+
   const userList = await User.findAll({
     attributes: ["id", "user_name", "email", "role", "ban"],
     raw: true,
@@ -13,7 +19,11 @@ module.exports.showUsers = async (req, res) => {
   //     console.log(user);
   //     console.log(user.user_name);
   //   });
-  res.render("management/manage_user", { title: "Manage Users", userList });
+  res.render("management/manage_user", {
+    title: "Manage Users",
+    userList,
+    admin,
+  });
 };
 
 module.exports.postManageUsers = async (req, res) => {
@@ -25,6 +35,9 @@ module.exports.postManageUsers = async (req, res) => {
           id: {
             [Op.in]: deleteIds,
           },
+          ban: {
+            [Op.gte]: banValue,
+          },
         },
       });
     }
@@ -32,7 +45,7 @@ module.exports.postManageUsers = async (req, res) => {
     if (typeof req.body.makeAdminIds !== "undefined") {
       const makeAdminIds = req.body.makeAdminIds;
       await User.update(
-        { role: "admin" },
+        { role: "admin", ban: 0 },
         {
           where: {
             id: {
